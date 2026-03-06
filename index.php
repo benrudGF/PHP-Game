3e8a29b9e6ba04a526b816b2958b079dc6180cf3
benrudGF <160250397+benrudGF@users.noreply.github.com>
2026-03-06 19:33:19 +0000

    Added Admin Link in Nav

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
                   <a href="index.php">Dashboard</a>
                   <a href="leaderboard.php">Leaderboard</a>
                   <a href="about.php">Market Rules</a>
+                  <a href="about.php">Admin</a>
               </nav>
           </div>
       </header>
